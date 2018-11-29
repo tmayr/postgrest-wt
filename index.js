4c1f81075cefc2e9fa885fbba81acfb8968a5133
Tomas Mayr <tmayr@tmayr.com>
2018-11-29 17:20:09 -0300

    reinconroporated psqlrst to index.js as we cannot have multiple files on webtask

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const url = require("url");
+const fs = require("fs");
+const { spawn, execSync } = require("child_process");
+const { promisify } = require("util");
+const writeFileAsync = promisify(fs.writeFile);
 const express = require("express");
 const proxy = require("express-http-proxy");
 const Webtask = require("webtask-tools");
 const serveIndex = require("serve-index");
-const PGR = require("./postgrest");
 
 const app = express();
+const PLATFORM = execSync("uname")
+  .toString()
+  .trim()
+  .toLowerCase();
 
 /**
  * since I'm clueless about where WT stores the code
@@ -40,6 +48,74 @@ app.use((err, req, res, next) => {
   console.log(err);
   return res.status(500).json({ message: err.toString() });
 });
+
+// PSQLRST Definitions
+const PGR = {
+  proc: null,
+  _writeConfig: async function({
+    db_uri = null,
+    schema = "public",
+    role = null
+  }) {
+    if (!db_uri) throw new Error("specify db uri as X-PSQLRST-DB-URI header");
+
+    const parsedURI = url.parse(db_uri);
+
+    if (!parsedURI.hostname) throw new Error("invalid db uri");
+
+    // prone to errors
+    if (!role) role = url.parse(db_uri).auth.split(":")[0];
+
+    const config = `
+    db-uri = "${db_uri}"
+    db-schema = "${schema}"
+    db-anon-role = "${role}"
+    db-pool = 1
+    
+    server-host = "127.0.0.1"
+    server-port = 3000
+    `;
+
+    const timestamp = new Date().getTime();
+    const filePath = `/tmp/psqlrst-${timestamp}.conf`;
+    await writeFileAsync(filePath, config);
+
+    return filePath;
+  },
+  connect: async function(config) {
+    // kill the session established beforehand
+    this.kill();
+
+    // output the configuration to a file
+    const configFilePath = await this._writeConfig(config);
+
+    return new Promise((resolve, reject) => {
+      // const binary = `/data/_verquire/postgrest-npm/1.0.0/node_modules/postgrest-npm/bin/postgrest-${PLATFORM}`;
+      const binary = `./node_modules/postgrest-npm/bin/postgrest-${PLATFORM}`;
+
+      // just overwriting a file might lead to security issues if any info leaks
+      this.proc = spawn(binary, [configFilePath]);
+
+      // log any errors
+      this.proc.stderr.on("data", data => {
+        const stringData = data.toString();
+        console.error("[stderr]", stringData);
+        reject(stringData);
+        this.kill();
+      });
+
+      // log any data incoming and listen for a string to determine if connection is done
+      this.proc.stdout.on("data", data => {
+        const stringData = data.toString();
+        console.log("[stdout]: ", stringData);
+        if (stringData.includes("Connection successful")) resolve(stringData);
+      });
+    });
+  },
+  kill: async function() {
+    if (this.proc) this.proc.kill();
+  }
+};
 
 module.exports = Webtask.fromExpress(app);
 
